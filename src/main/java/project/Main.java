4f1fd4bfa7d9a5844429b48a30df178871385bf8
Ada <lowskabo@gmail.com>
2023-07-02 14:12:21 +0200

    dni tygodnia - całość

---
+++ src/main/java/project/Main.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DayOfWeek dayOfWeek = new DayOfWeek();
+
+        int dayNumber = 5;
+        String dayOfWeekName = dayOfWeek.getWeekDay(dayNumber);
+
+        System.out.println("Day of the week: " + dayOfWeekName);
+    }
+}
