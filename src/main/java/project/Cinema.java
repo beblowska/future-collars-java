c745ef069c7e1c959f5fe2bed7fb665a849a246e
Ada <lowskabo@gmail.com>
2023-07-31 16:55:54 +0200

    zadanie 1 - kino

---
+++ src/main/java/project/Cinema.java
@@ -0,0 +1,30 @@
+package project;
+
+public class Cinema {
+
+    private final int seatNumber;
+    private final int rowNumber;
+    private boolean isOccupied;
+
+    public Cinema(int seatNumber, int rowNumber) {
+        this.seatNumber = seatNumber;
+        this.rowNumber = rowNumber;
+        this.isOccupied = false;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+    public int getRowNumber() {
+        return rowNumber;
+    }
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+    public void occupy() {
+        isOccupied = true;
+    }
+    public void free() {
+        isOccupied = false;
+    }
+}
---
+++ src/main/java/project/Main.java
@@ -0,0 +1,16 @@
+package project;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cinema cinema = new Cinema(1, 2);
+        System.out.println("Seat number: " + cinema.getSeatNumber());
+        System.out.println("Row number: " + cinema.getRowNumber());
+
+        cinema.occupy();
+        System.out.println("Occupied: " + cinema.isOccupied());
+
+        cinema.free();
+        System.out.println("Free: " + cinema.isOccupied());
+    }
+}
