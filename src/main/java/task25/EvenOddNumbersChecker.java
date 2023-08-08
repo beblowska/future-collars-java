a1315c7a55d21b39cf9fbe5585afc0484bffd849
Ada <lowskabo@gmail.com>
2023-08-08 10:39:27 +0200

    Zmiana nazwy pakietu, korekta testów

--- src/main/java/task25/EvenOddNumbersChecker.java
+++ src/main/java/task25/EvenOddNumbersChecker.java
@@ -1,12 +1,14 @@
-package project;
+package task25;
 
 public class EvenOddNumbersChecker {
 
     public boolean isEven(int number) {
+
         return number % 2 == 0;
     }
 
     public boolean isOdd(int number) {
+
         return number % 2 != 0;
     }
 }
--- src/test/java/task25/EvenOddNumbersCheckerTest.java
+++ src/test/java/task25/EvenOddNumbersCheckerTest.java
@@ -1,6 +1,7 @@
-package project;
+package task25;
 
 import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EvenOddNumbersCheckerTest {
@@ -9,16 +10,12 @@ public class EvenOddNumbersCheckerTest {
     public void shouldTestEvenNumbers() {
         EvenOddNumbersChecker checker = new EvenOddNumbersChecker();
         assertTrue(checker.isEven(2));
-        assertTrue(checker.isEven(20));
-        assertTrue(checker.isEven(-10));
     }
 
     @Test
     public void shouldTestOddNumbers() {
         EvenOddNumbersChecker checker = new EvenOddNumbersChecker();
         assertTrue(checker.isOdd(3));
-        assertTrue(checker.isOdd(15));
-        assertTrue(checker.isOdd(-27));
     }
 
 }
