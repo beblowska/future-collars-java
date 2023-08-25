ba66c077c2df63bdf580bedf9eaecf9b15fa2337
Ada <lowskabo@gmail.com>
2023-08-25 22:22:10 +0200

    Rozbicie testów

--- src/test/java/task48/PersonTest.java
+++ src/test/java/task48/PersonTest.java
@@ -6,27 +6,35 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PersonTest {
 
     @Test
-    public void testEquals() {
+    public void shouldBeEqual() {
         Person person1 = new Person("Adam", 30);
         Person person2 = new Person("Adam", 30);
-        Person person3 = new Person("Alicja", 25);
 
-        assertTrue(person1.equals(person2));
-        assertTrue(person2.equals(person1));
-
-        assertFalse(person1.equals(person3));
-        assertFalse(person3.equals(person1));
-
-        assertTrue(person1.equals(person1));
+        assertEquals(person1, person2);
+        assertEquals(person2, person1);
+        assertEquals(person1, person1);
     }
 
     @Test
-    public void testHashCode() {
+    public void shouldNotBeEqual() {
         Person person1 = new Person("Adam", 30);
-        Person person2 = new Person("Adam", 30);
         Person person3 = new Person("Alicja", 25);
 
+        assertNotEquals(person1, person3);
+        assertNotEquals(person3, person1);
+    }
+    @Test
+    public void shouldBedHashCodeForEqualPersons() {
+        Person person1 = new Person("Adam", 30);
+        Person person2 = new Person("Adam", 30);
+
         assertEquals(person1.hashCode(), person2.hashCode());
+    }
+
+    @Test
+    public void shouldBeHashCodeForDifferentPersons() {
+        Person person1 = new Person("Adam", 30);
+        Person person3 = new Person("Alicja", 25);
 
         assertNotEquals(person1.hashCode(), person3.hashCode());
     }
