8096a31cf0fc369d405a72200cbf70237a400fbe
beblowska <137942338+beblowska@users.noreply.github.com>
2023-08-26 21:32:31 +0200

    Update NumberOfCharacters

--- src/main/java/task63/NumberOfCharacters.java
+++ src/main/java/task63/NumberOfCharacters.java
@@ -1,14 +1,11 @@
 package task63;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class NumberOfCharacters {
-    public static void main(String[] args) throws FileNotFoundException {
-        String filePath = ("src/main/resources63/data.txt");
-
+    public static int countCharacters(String filePath) {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             int characterCount = 0;
             int data;
@@ -17,7 +14,7 @@ public class NumberOfCharacters {
                 characterCount++;
             }
 
-            System.out.println("Number of characters: " + characterCount);
+            return characterCount;
         } catch (IOException exception) {
             exception.printStackTrace();
         }
