80299eed1635aed823635a2fc70da2324f295cdb
beblowska <137942338+beblowska@users.noreply.github.com>
2023-08-26 21:44:20 +0200

    Update WordSearch

--- src/main/java/task64/WordSearch.java
+++ src/main/java/task64/WordSearch.java
@@ -5,25 +5,20 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class WordSearch {
-    public static void main(String[] args) {
-        String filePath = "src/main/resources64/data.txt";
-        String searchWord = "Java";
+    public static int countOccurrences(String filePath, String searchWord) {
         int wordCount = 0;
-
-        System.out.println("Search word: " + searchWord);
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
 
-
             while ((line = reader.readLine()) != null) {
                 if (line.contains(searchWord)) {
                     wordCount++;
-                    System.out.println("Word found " + wordCount + " times.");
                 }
             }
         } catch (IOException exception) {
             exception.printStackTrace();
         }
-    }
+
+        return wordCount;
 }
