fdf6d58b2741389d95f810ce8c5895d20e1fcd10
beblowska <137942338+beblowska@users.noreply.github.com>
2023-08-26 20:30:11 +0200

    Update TextProcessor.java
    Zmiana metody na publiczną, a ciało realizuje polecenia

--- src/main/java/task51/TextProcessor.java
+++ src/main/java/task51/TextProcessor.java
@@ -1,18 +1,35 @@
 package task51;
 
 public class TextProcessor {
-    public static boolean containsAnyCharacters(String text) {
+    public static void processText(String text) {
+        if (containsAnyCharacters(text)) {
+            System.out.println("The text contains characters: " + text);
+
+            if (isPalindrome(text)) {
+                System.out.println("The text is palindrome.");
+            } else {
+                System.out.println("The text is not a palindrome.");
+            }
+
+            int length = calculateLength(text);
+            System.out.println("The length of the text is: " + length);
+        } else {
+            System.out.println("No text.");
+        }
+    }
+
+    private static boolean containsAnyCharacters(String text) {
         return text != null && !text.isEmpty();
     }
 
-    public static boolean isPalindrome(String text) {
+    private static boolean isPalindrome(String text) {
         if (text == null) {
             return false;
         }
 
         text = text.replaceAll("[^a-zA-Z]", "").toLowerCase();
         int left = 0;
-        int right = text.length() -1;
+        int right = text.length() - 1;
 
         while (left < right) {
             if (text.charAt(left) != text.charAt(right)) {
@@ -24,7 +41,7 @@ public class TextProcessor {
         return true;
     }
 
-    public static int calculateLength(String text) {
+    private static int calculateLength(String text) {
         if (text == null) {
             return 0;
         }
