19a0116a70cc36c044e19750a2f835053f9b412a
beblowska <137942338+beblowska@users.noreply.github.com>
2023-08-26 21:16:25 +0200

    Update Main

--- src/main/java/task60/Main.java
+++ src/main/java/task60/Main.java
@@ -1,27 +1,11 @@
 package task60;
 
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
+import static task60.WriteToFile.saveToFile;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("src/main/resources/data.txt");
-
-        String fileName = file.getName();
-        boolean isReadable = file.canRead();
-        boolean exists = file.exists();
-
         String content = "Kurs\nJava\nLekcja 6\nPliki\nWyjątki\nPliki\nKoniec pliku";
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("data.txt"))) {
-            writer.write(content);
-
-    } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        String filePath = "src/main/resources/data.txt";
+        saveToFile(content, filePath);
     }
 }
