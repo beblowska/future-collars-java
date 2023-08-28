792725be40f2900e29a05302737da92cfa547ec0
beblowska <137942338+beblowska@users.noreply.github.com>
2023-08-28 19:31:15 +0200

    Update SeasonMapperTest

--- src/test/java/task75/SeasonMapperTest.java
+++ src/test/java/task75/SeasonMapperTest.java
@@ -1,24 +1,39 @@
 package task75;
 
 import java.time.Month;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class SeasonMapperTest {
-    @Test
-    public void testGetSeason() {
-        assertEquals(Season.WINTER, SeasonMapper.mapToSeason(Month.DECEMBER));
-        assertEquals(Season.WINTER, SeasonMapper.mapToSeason(Month.JANUARY));
-        assertEquals(Season.WINTER, SeasonMapper.mapToSeason(Month.FEBRUARY));
-        assertEquals(Season.SPRING, SeasonMapper.mapToSeason(Month.MARCH));
-        assertEquals(Season.SPRING, SeasonMapper.mapToSeason(Month.APRIL));
-        assertEquals(Season.SPRING, SeasonMapper.mapToSeason(Month.MAY));
-        assertEquals(Season.SUMMER, SeasonMapper.mapToSeason(Month.JUNE));
-        assertEquals(Season.SUMMER, SeasonMapper.mapToSeason(Month.JULY));
-        assertEquals(Season.SUMMER, SeasonMapper.mapToSeason(Month.AUGUST));
-        assertEquals(Season.FALL, SeasonMapper.mapToSeason(Month.SEPTEMBER));
-        assertEquals(Season.FALL, SeasonMapper.mapToSeason(Month.OCTOBER));
-        assertEquals(Season.FALL, SeasonMapper.mapToSeason(Month.NOVEMBER));
+    @ParameterizedTest
+    @EnumSource(Month.class)
+    public void testGetSeason(Month month) {
+        Season expectedSeason = mapMonthToSeason(month);
+        assertEquals(expectedSeason, SeasonMapper.mapToSeason(month));
+    }
+
+    private Season mapMonthToSeason(Month month) {
+        switch (month) {
+            case DECEMBER:
+            case JANUARY:
+            case FEBRUARY:
+                return Season.WINTER;
+            case MARCH:
+            case APRIL:
+            case MAY:
+                return Season.SPRING;
+            case JUNE:
+            case JULY:
+            case AUGUST:
+                return Season.SUMMER;
+            case SEPTEMBER:
+            case OCTOBER:
+            case NOVEMBER:
+                return Season.FALL;
+            default:
+                throw new IllegalArgumentException("Invalid month.");
+        }
     }
 }
