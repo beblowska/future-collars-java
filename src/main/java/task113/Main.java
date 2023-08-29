28b9d671fdb6b971b6aeebe175184a4a951d942c
beblowska <137942338+beblowska@users.noreply.github.com>
2023-08-29 14:59:21 +0200

    Update Main

--- src/main/java/task113/Main.java
+++ src/main/java/task113/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         CinemaManager cinemaManager = new CinemaManager();
 
-        Map<String, Long> hallCountByType = cinemaManager.countHallsByType();
+        Map<HallType, Long> hallCountByType = cinemaManager.countHallsByType();
 
         System.out.println("Cinema Halls by type:");
         hallCountByType.forEach((type, count) -> System.out.println(type + ": " + count));
