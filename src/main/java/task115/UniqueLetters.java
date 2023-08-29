cd430b92843867cbf757a02db5a3f27c5dd6bfaf
beblowska <137942338+beblowska@users.noreply.github.com>
2023-08-29 15:11:53 +0200

    Update UniqueLetters

--- src/main/java/task115/UniqueLetters.java
+++ src/main/java/task115/UniqueLetters.java
@@ -1,22 +1,15 @@
 package task115;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class UniqueLetters {
     public static Set<Character> findUniqueLetters(List<String> words) {
-        Set<Character> uniqueLetters = new HashSet<>();
-
-        for (String word : words) {
-            char[] letters = word.toCharArray();
-            for (char letter : letters) {
-                if (Character.isLetter(letter)) {
-                    uniqueLetters.add(Character.toUpperCase(letter));
-                }
-            }
-        }
-
-        return uniqueLetters;
+         return words.stream()
+                .flatMap(word -> word.chars().mapToObj(ch -> (char) ch))
+                .filter(Character::isLetter)
+                .map(Character::toUpperCase)
+                .collect(Collectors.toSet());
     }
 }
